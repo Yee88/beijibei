ba52c1028df425f41a4686b26d9554d57fe661ed
DingRan888 <972395150@qq.com>
2019-02-26 17:31:06 +0800

    guidecate

--- src/views/Word/Guide/GuideCate/index.js
+++ src/views/Word/Guide/GuideCate/index.js
@@ -1,10 +1,44 @@
 import React,{Component} from "react"
+import {Link} from "react-router-dom";
 import axios from "axios"
+import guideCateDetail from './index.module.scss';
 
 class GuideCate extends Component{
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	catedetaillist:[]
+	  };
+	}
     render(){
         return <div>
-            111
+            <div className={guideCateDetail.catedetail}>
+            	<div className={guideCateDetail.w}>
+	            	<div className={guideCateDetail.header}>
+	            		<h1>选择单词书<span>/{this.props}</span></h1>
+	            	</div>
+	            	<div className={guideCateDetail.datalist}>
+						<ul className={guideCateDetail.detaildata}>
+							{
+	        					this.state.catedetaillist.map(item=>
+	        						<li className={guideCateDetail.detaillist}>
+										<dl>
+											<dt><img src={item.cover_url}/></dt>
+											<dd>
+												<h3>{item.title}</h3>
+												<p>上传者：{item.owner.username}</p>
+												<p>单词数：{item.count}</p>
+												<button>开始学习</button>
+											</dd>
+										</dl>
+									</li>
+	        					)
+	        				}
+						</ul>
+	            	</div>
+	            </div>
+            </div>
         </div>
     }
     componentDidMount(){
@@ -13,8 +47,11 @@ class GuideCate extends Component{
     		url:`/api/v1/guide/wordbook/${this.props.match.params.id}/`
     	}).then(res=>{
     		console.log(res.data.data)
+    		this.setState({
+    			catedetaillist:res.data.data
+    		})
     	})
     }
-   
+
 }
 export default GuideCate
